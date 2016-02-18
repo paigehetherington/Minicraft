59a6072b4dc5b363f42ebb4b997fa7d895d4e20e
Paige Hetherington <herbmama@gmail.com>
2016-02-18 15:11:28 -0500

    made character move and stop

--- core/src/com/theironyard/minicraft/Minicraft.java
+++ core/src/com/theironyard/minicraft/Minicraft.java
@@ -2,6 +2,7 @@ package com.theironyard.minicraft;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -10,6 +11,9 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class Minicraft extends ApplicationAdapter {
     final int WIDTH = 100;
     final int HEIGHT = 100;
+    float xv, yv;
+    static final float MAX_VELOCITY = 100;
+
 
     SpriteBatch batch;
     TextureRegion down, up, right, left;
@@ -28,15 +32,98 @@ public class Minicraft extends ApplicationAdapter {
         right = grid[6][3];
         left = new TextureRegion(right);
         left.flip(true, false);
+
+
+
+
     }
 
     @Override
     public void render () {
+
+        float oldX = x;
+        float oldY = y;
+
+        move();
+        //to get dude to stop at edge:
+        //if (x < 0 || x > (Gdx.graphics.getWidth()-WIDTH)) {
+           // x = oldX;
+        //}
+        //if (y < 0 || y > (Gdx.graphics.getHeight()-HEIGHT)){
+            //y = oldY;
+        //}
+        //to get dude to teleport:
+        if (x < 0) {
+            x = Gdx.graphics.getWidth()-WIDTH;
+        }
+        else if (x > Gdx.graphics.getWidth()-WIDTH) {
+            x = 0;
+        }
+        if (y < 0) {
+            y = Gdx.graphics.getHeight()-HEIGHT;
+        } else if (y > Gdx.graphics.getHeight()-HEIGHT) {
+           y = 0;
+        }
+
+
+        //if the dude's x is > Gdx.graphics.getWidth, stop him
+        //same for y
+        //you could stop his velocity
+        // we have his x and y at all times
+
+
+
+
+
+        TextureRegion img;
+        img = down;
+    if (Math.abs(yv) > Math.abs(xv)) {
+        if (yv >= 0) {
+            img = up;
+        } else {
+            img = down;
+        }
+    } else {
+        if (xv >= 0) {
+            img = right;
+        } else {
+            img = left;
+        }
+    }
+
+
         Gdx.gl.glClearColor(0, 0.5f, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         batch.begin();
-        batch.draw(down, x, y, WIDTH, HEIGHT);
+        batch.draw(img, x, y, WIDTH, HEIGHT);
         batch.end();
     }
+    void move() {
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            yv = MAX_VELOCITY;
+
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            yv = MAX_VELOCITY * -1;
+
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            xv = MAX_VELOCITY;
+
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            xv = MAX_VELOCITY * -1;
+        }
+
+        y += yv *Gdx.graphics.getDeltaTime(); //delta time - amt of time since last frame ran. yv= # pixels movein 1 second
+        x += xv * Gdx.graphics.getDeltaTime();
+        xv = xv * 0.3f;
+        yv = yv * 0.3f;
+
+
+        //yv = decelerate(yv);
+        //xv = decelerate(xv);
+    }
+
 }
